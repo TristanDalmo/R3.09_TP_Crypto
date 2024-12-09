a6615ae6fbeed10dad797cddfe841de3856d9753
md882742 <matteo.de-marco@iut-dijon.u-bourgogne.fr>
2024-12-09 10:59:54 +0100

    pBox implémenté

--- CryptoEffectClient/Algorithmes/Realisations/AlgorithmeFeistel.cs
+++ CryptoEffectClient/Algorithmes/Realisations/AlgorithmeFeistel.cs
@@ -56,7 +56,46 @@ namespace CryptoEffectClient.Algorithmes.Realisations
 
         public string Pbox(string message)
         {
-            throw new NotImplementedException();
+            string messageSortie = "";
+            List<char> sortie = new List<char>();
+            sortie.Add(message[15]);
+            sortie.Add(message[6]);
+            sortie.Add(message[19]);
+            sortie.Add(message[20]);
+            sortie.Add(message[28]);
+            sortie.Add(message[11]);
+            sortie.Add(message[27]);
+            sortie.Add(message[16]);
+            sortie.Add(message[0]);
+            sortie.Add(message[14]);
+            sortie.Add(message[22]);
+            sortie.Add(message[25]);
+            sortie.Add(message[4]);
+            sortie.Add(message[17]);
+            sortie.Add(message[30]);
+            sortie.Add(message[9]);
+            sortie.Add(message[1]);
+            sortie.Add(message[7]);//l18
+            sortie.Add(message[23]);
+            sortie.Add(message[13]);
+            sortie.Add(message[31]);
+            sortie.Add(message[26]);//22
+            sortie.Add(message[2]);
+            sortie.Add(message[8]);
+            sortie.Add(message[18]);
+            sortie.Add(message[12]);//26
+            sortie.Add(message[29]);
+            sortie.Add(message[5]);
+            sortie.Add(message[21]);//29
+            sortie.Add(message[10]);
+            sortie.Add(message[3]);
+            sortie.Add(message[24]);
+            foreach (char c  in sortie)
+            {
+                messageSortie += c;
+            }
+            return messageSortie;
+
         }
 
         public string Sbox(string message)
